1bb2aa7172d6388e0d663d52695deb218e33408d
Viktor Persson <vikpe@users.noreply.github.com>
2023-04-12 22:23:30 +0200

    Add WIP label.

--- src/features/CharsetEditor/App.vue
+++ src/features/CharsetEditor/App.vue
@@ -9,7 +9,13 @@ import CharsetEditor from "./CharsetEditor.vue";
   <div class="bg-gray-100 border-b border-gray-300">
     <div class="bg-white shadow">
       <div class="container">
-        <h1 class="font-bold text-xl py-4">Charset Editor</h1>
+        <h1 class="font-bold text-xl py-4">
+            Charset Editor
+            <span
+              class="rounded-lg bg-yellow-200 font-normal px-2 py-1 ml-4 border text-sm"
+              >work in progress!</span
+            >
+        </h1>
       </div>
     </div>
 
